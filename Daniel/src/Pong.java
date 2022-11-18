bfb5cf7058fea0217daed62577e98a9f847f3e96
jianruan <ruanj219@wfu.edu>
2022-11-18 17:51:49 -0500

    Finished Individual Work

---
+++ Daniel/src/Pong.java
@@ -0,0 +1,19 @@
+/***********************************************************************
+ * @file Pong.java
+ * @brief I created a 2-player Pong Game instead of a 1-player vs computer Pong Game.
+ * @author Daniel Ruan
+ * @date: 11/18/22
+ * @acknowledgement: None
+ ***********************************************************************/
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Pong {
+    public static void main (String[] args) {
+        GameFrame frame = new GameFrame();
+    }
+}
+
