ffe950ac77097c43374bece869ff5838b57b046f
Sabrina Guan <guanx19@wfu.edu>
2022-11-29 17:03:03 -0500

    Final Individual work

--- Sabrina/src/Ball.java
+++ Sabrina/src/Ball.java
@@ -3,54 +3,63 @@ import java.awt.*;
 public class Ball {
     double xVel, yVel, x, y;
 
-    public Ball(){
+    public Ball() {
         x = 350;
         y = 250;
         xVel = getRandomSpeed() * getRandomDirection();
         yVel = getRandomSpeed() * getRandomDirection();
+
     }
 
-    public double getRandomSpeed(){
-        return (Math.random() * 3 + 2);
+    public double getRandomSpeed() {
+        return (Math.random() * 3+1);
+
     }
 
-    public int getRandomDirection(){
+    public double getRandomDirection() {
         int rand = (int) (Math.random() * 2);
-        if (rand ==1)
+        if (rand == 1) {
             return 1;
-        else
+        } else
             return -1;
     }
-    public void draw(Graphics g){
+
+    public void draw(Graphics g) {
         g.setColor(Color.white);
-        g.fillOval((int)x-10, (int)y-10,20,20);
+        g.fillOval((int) x - 10, (int) y - 10, 20, 20);
     }
 
-    public void checkPaddleCollision(Paddle p1, Paddle p2){
-        if (x <= 50){
-            if (y >= p1.getY() && y <= p1.getY() + 80)
+    public void checkPaddleCollision(Paddle p1, Paddle p2) {
+        if (x <= 50) {
+            if (y >= p1.getY() && y <= p1.getY() + 80) {
                 xVel = -xVel;
-        }
-        else if (x >= 650){
-            if (y >= p2.getY() && y <= p2.getY() + 80)
+            }
+        } else if (x >= 650) {
+            if (y >= p2.getY() && y <= p2.getY() + 80) {
                 xVel = -xVel;
+            }
         }
     }
 
-    public void move(){
+    public void move() {
         x += xVel;
         y += yVel;
 
-        if (y<10)
+        if (y < 10) {
             yVel = -yVel;
-        if (y > 490)
+        }
+        if (y > 490) {
             yVel = -yVel;
+        }
     }
 
-    public int getX(){
-        return (int)x;
+    public int getX() {
+        return (int) x;
     }
-    public int getY(){
-        return (int)y;
+
+    public int getY() {
+        return (int) y;
     }
+
+
 }
--- Sabrina/src/Tennis.java
+++ Sabrina/src/Tennis.java
@@ -10,77 +10,94 @@ public class Tennis extends JFrame implements Runnable, KeyListener {
     AIPaddle p2;
     Ball b1;
     boolean gameStarted;
-    Graphics g;
 
-    public Tennis(){
+
+    public Tennis() {
         this.setSize(WIDTH, HEIGHT);
         gameStarted = false;
-        this.setTitle("Pong Game");
-        this.setVisible(true);
-        this.addKeyListener(this);
+
         p1 = new HumanPaddle(1);
         b1 = new Ball();
-        p2 = new AIPaddle(2, b1);
+        p2 = new AIPaddle(2,  b1);
+
+        this.setTitle("Pong Game");
+
         thread = new Thread(this);
         thread.start();
+        this.setVisible(true);
+        this.addKeyListener(this);
     }
-
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
         g.setColor(Color.BLACK);
-        g.fillRect(0,0,WIDTH,HEIGHT);
-        if(b1.getX() < -10 || b1.getX() > 710){
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        if (b1.getX() < -10 || b1.getX() > 710) {
             g.setColor(Color.red);
-            g.drawString("Game Over",350,250);
-        }
-        else {
+            g.drawString("Game Over", 350, 250);
+        } else {
             p1.draw(g);
             b1.draw(g);
             p2.draw(g);
         }
-        if(!gameStarted){
+//        p1.draw(g);
+//        b1.draw(g);
+//        p2.draw(g);
+
+        if (!gameStarted) {
             g.setColor(Color.white);
             g.drawString("Tennis", 340, 100);
-            g.drawString("Press Enter to Begin..", 310, 130);
+            g.drawString("Press Enter to begin...", 310, 130);
         }
     }
 
-    public void update(Graphics g){
-        paint(g);
+
+    public void update (Graphics g) {
+        paint (g);
+    }
+
+    public void repaint(Graphics g) {
+        repaint(g);
     }
     public void run(){
-        for(;;){
+        for (;;) {
             if (gameStarted) {
+
                 p1.move();
                 p2.move();
                 b1.move();
-                b1.checkPaddleCollision(p1, p1);
+                b1.checkPaddleCollision(p1, p2);
             }
-            repaint();
+            this.repaint();
+
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
         }
     }
-    public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode() == KeyEvent.VK_UP){
+
+    public void keyPressed (KeyEvent e) {
+        if (e.getKeyCode()== KeyEvent.VK_UP) {
             p1.setUpAccel(true);
-        } else if (e.getKeyCode () == KeyEvent.VK_DOWN){
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
             p1.setDownAccel(true);
         } else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
             gameStarted = true;
         }
     }
-    public void keyReleased(KeyEvent e){
-        if(e.getKeyCode() == KeyEvent.VK_UP){
+
+    public void keyReleased (KeyEvent e) {
+        if (e.getKeyCode () == KeyEvent.VK_UP) {
             p1.setUpAccel(false);
-        } else if (e.getKeyCode () == KeyEvent.VK_DOWN){
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
             p1.setDownAccel(false);
         }
     }
+
     public void keyTyped(KeyEvent arg0) {
 
     }
+
+
+
 }
